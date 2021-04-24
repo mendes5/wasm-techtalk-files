c6bec869bdce487c77205116340c1b9d45912761
Fernando Mendes <fernando.mendes@tagview.com.br>
2021-04-24 13:37:45 -0300

    Renames

--- Act 1/2-add-function.c
+++
@@ -1,6 +0,0 @@
-#define WASM_EXPORT __attribute__((visibility("default")))
-
-WASM_EXPORT
-float add(float x, float y) {
-  return x + y;
-}
--- Act 1/index.js
+++
@@ -1,8 +0,0 @@
-fetch("../out/main.wasm")
-  .then((response) => response.arrayBuffer())
-  .then((bytes) => WebAssembly.instantiate(bytes))
-  .then(({ instance }) => {
-      const result = instance.exports.fn();
-      console.log({ result });
-  })
-  .catch(console.error);
